d4922de20270612c7274c054c16d6fbe61721a26
Sunit Singh <sunit.iit.roorkee@gmail.com>
2012-04-21 22:07:09 +0530

    2

--- Triplet.java
+++ Triplet.java
@@ -9,7 +9,7 @@ import java.math.BigInteger;
 import java.io.InputStream;
 
 
-public class Format
+public class Triplet
 {
         public static void main(String[] Args)
         {
@@ -17,18 +17,71 @@ public class Format
                 OutputStream outputStream = System.out;
                 InputReader in = new InputReader(inputStream);
                 PrintWriter out = new PrintWriter(outputStream);
-                Petr solver = new Petr();
+                CodeTriplet solver = new CodeTriplet();
                 solver.solve(1, in, out);
                 out.close();
         }
 }
 
-class Petr 
+class CodeTriplet
 {
 	public void solve(int testNumber, InputReader in, PrintWriter out)
 	{
-		
-
+		int n=in.nextInt();
+		double arr[][]=new double[n][2];
+		in.getArray(arr);
+		int c=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				for(int k=j+1;k<n;k++)
+				{
+					double x=0;
+					double y=0;
+					x=(arr[j][0]+arr[k][0])/2;
+					y=(arr[j][1]+arr[k][1])/2;
+					if(x==arr[i][0]&&y==arr[i][1])
+					{
+						c++;
+						/*out.println(x+" "+y);
+						out.println(arr[j][0]+" "+arr[j][1]);
+						out.println(arr[k][0]+" "+arr[k][1]);
+						out.println("Here");
+						* */
+					}
+					x=0;
+					y=0;
+					x=(arr[i][0]+arr[k][0])/2;
+					y=(arr[i][1]+arr[k][1])/2;
+					if(x==arr[j][0]&&y==arr[j][1])
+					{
+						c++;
+						/*
+						out.println(x+" "+y);
+						out.println(arr[i][0]+" "+arr[i][1]);
+						out.println(arr[k][0]+" "+arr[k][1]);
+						out.println("Here2");
+						* */
+					}
+					x=0;
+					y=0;
+					x=(arr[j][0]+arr[i][0])/2;
+					y=(arr[j][1]+arr[i][1])/2;
+					if(x==arr[k][0]&&y==arr[k][1])
+					{
+						c++;
+						/*
+						out.println(x+" "+y);
+						out.println(arr[i][0]+" "+arr[i][1]);
+						out.println(arr[j][0]+" "+arr[j][1]);
+						out.println("Here3");
+						* */
+					}
+				}
+			}
+		}
+		out.println(c);
 
 
 
@@ -49,14 +102,7 @@ class Petr
 			out.println(arr[i]);
 		}
 	}
-	public void printArray(double arr[],PrintWriter out)
-	{
-		for(int i=0;i<arr.length;i++)
-		{
-			out.println(arr[i]);
-		}
-	}
-	public void printArray(String arr[],PrintWriter out)
+	public void printArray(float arr[],PrintWriter out)
 	{
 		for(int i=0;i<arr.length;i++)
 		{
@@ -110,20 +156,30 @@ class InputReader
     }
     public double nextDouble()
     {
-        return Double.parseDouble(next());
-    }
-    public void getArray(int arr[])
+		return Double.parseDouble(next());
+	}
+    public void getArray(int arr[][])
     {
 		for(int i=0;i<arr.length;i++)
 		{
-			arr[i]=nextInt();
+			arr[i][0]=nextInt();
+			arr[i][1]=nextInt();
 		}
 	}
-	public void getArray(double arr[])
+	public void getArray(long arr[][])
     {
 		for(int i=0;i<arr.length;i++)
 		{
-			arr[i]=nextDouble();
+			arr[i][0]=nextLong();			
+			arr[i][1]=nextLong();			
+		}
+	}
+	public void getArray(double arr[][])
+    {
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i][0]=nextDouble();			
+			arr[i][1]=nextDouble();			
 		}
 	}
 	public void getArray(String arr[])
@@ -131,36 +187,6 @@ class InputReader
 		for(int i=0;i<arr.length;i++)
 		{
 			arr[i]=next();
-		}
-	}
-	public void getArray2D(String arr[][])
-    {
-		for(int i=0;i<arr.length;i++)
-		{
-			for(int j=0;j<arr[i].length;j++)
-			{
-				arr[i][j]=next();
-			}
-		}
-	}
-	public void getArray2D(int arr[][])
-    {
-		for(int i=0;i<arr.length;i++)
-		{
-			for(int j=0;j<arr[i].length;j++)
-			{
-				arr[i][j]=nextInt();
-			}
-		}
-	}
-	public void getArray2D(double arr[][])
-    {
-		for(int i=0;i<arr.length;i++)
-		{
-			for(int j=0;j<arr[i].length;j++)
-			{
-				arr[i][j]=nextDouble();
-			}
 		}
 	}
 }
